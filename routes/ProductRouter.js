78484b72001d57458189a44237749924d3ebe649
an.lee1308 <an.lee1308@example.com>
2022-03-09 23:08:15 +0700

    find sort paninate products

--- routes/ProductRouter.js
+++ routes/ProductRouter.js
@@ -7,5 +7,6 @@ Router.get(
   Authentication.isAdmin,
   ProductController.getAllProduct
 ); //get product by search or get all product
-
+Router.get("/products", ProductController.getProduct); //get product by search or get all product
+Router.get("/products/:category", ProductController.getProductByCategory); // get product by category
 module.exports = Router;
